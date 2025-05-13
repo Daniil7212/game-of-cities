f5d21499f6ed5ca3c899567129e99818423fcd5c
Daniil7212 <122516686+Daniil7212@users.noreply.github.com>
2025-05-13 22:58:54 +0500

    Many players!!!

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ file.close()
 games = {}
 players = {}
 opponent = {}
+tele_id = {}
 answers = ['Такой город действительно есть', 'Ок', 'Хорошо', 'И вправду', 'Хороший выбор' 'Very good']
 
 
@@ -66,69 +67,98 @@ def start(message):
     bot.send_message(message.chat.id,
                      """
                     Это игра в Города
-                    Напишите '/play' чтобы создать игру
-                    Или '/join [ID_игры]' чтобы присоединиться к игре
+                    Напишите '/create' чтобы создать игру
+                    Или '/join ID_игры' чтобы присоединиться к игре
                     """)
 
 
-@bot.message_handler(commands=['resign'])
-def resign(message):
-    user_name = message.from_user.username
-    bot.send_message(message.chat.id, "Ок, вы проиграли")
-    try:
-        bot.send_message(opponent[user_name]["id"], "Ваш соперник сдался. Поздравляем, вы выиграли!")
-
-        del players[opponent[user_name]["name"]]
-        del opponent[opponent[user_name]["name"]]
-        del opponent[user_name]["name"]
-    except KeyError:
-        pass
-
-    del games[players[user_name]]
-    del players[user_name]
-
-
-@bot.message_handler(commands=['play'])
-def play(message):
+@bot.message_handler(commands=['create'])
+def create(message):
     game_id = str(random.randint(10000, 99999))
+
     bot.send_message(message.chat.id, f"Игра создана")
-    bot.send_message(message.chat.id, f"Если захотите сдаться или выйти из игры напишите '/resign'")
     bot.send_message(message.chat.id, f"ID игры: {game_id}")
+    bot.send_message(message.chat.id, f"Когда все игроки присоединятся напишите '/play' чтобы начать")
+    bot.send_message(message.chat.id, f"Если захотите выйти из игры напишите '/leave'")
+
     players[message.from_user.username] = game_id
+    tele_id[message.from_user.username] = message.from_user.id
     games[game_id] = {
         "cities": [],
-        "first_player_id": message.from_user.id,
-        "first_player_name": message.from_user.username,
+        "players": [message.from_user.username, ],
         "turn": None,
         "last_char": None
     }
 
 
+@bot.message_handler(commands=['leave'])
+def leave(message):
+    if len(games[players[message.from_user.username]]["players"]) <= 2:
+        bot.send_message(message.from_user.id, "Хорошо, мы удалили вас из игры")
+
+        del tele_id[message.from_user.username]
+        games[players[message.from_user.username]]["players"].remove(message.from_user.username)
+
+        if len(games[players[message.from_user.username]]["players"]) == 1:
+            pl = games[players[message.from_user.username]]["players"][0]
+
+            bot.send_message(tele_id[pl], "Ваш последний противник вышел из игры. Поздравляем, вы выиграли!")
+
+            del players[pl]
+            del tele_id[pl]
+
+        del games[players[message.from_user.username]]
+        del players[message.from_user.username]
+
+    else:
+        bot.send_message(message.from_user.id, "Хорошо, мы удалили вас из игры")
+        del tele_id[message.from_user.username]
+        games[players[message.from_user.username]]["players"].remove(message.from_user.username)
+
+        for i in games[players[message.from_user.username]]["players"]:
+            bot.send_message(tele_id[i], f"Из игры вышел игрок: {message.from_user.username}")
+
+        del players[message.from_user.username]
+
+
 @bot.message_handler(commands=['join'])
 def join(message):
     if len(message.text.split()) != 2:
-        bot.send_message(message.chat.id, "Укажите ID игры: `/join [ID_игры]`")
+        bot.send_message(message.chat.id, "Укажите ID игры: `/join ID_игры`")
 
     game_id = str(message.text.split()[1])
 
     if game_id not in games:
         bot.send_message(message.chat.id, "Игра не найдена")
 
-    games[game_id]["second_player_id"] = message.from_user.id
-    games[game_id]["second_player_name"] = message.from_user.username
-    games[game_id]["turn"] = message.from_user.username
-    players[message.from_user.username] = game_id
-    opponent[message.from_user.username] = {}
-    opponent[message.from_user.username]["name"] = games[game_id]["first_player_name"]
-    opponent[games[game_id]["first_player_name"]] = {}
-    opponent[games[game_id]["first_player_name"]]["name"] = message.from_user.username
-    opponent[message.from_user.username]["id"] = games[game_id]["first_player_id"]
-    opponent[games[game_id]["first_player_name"]]["id"] = message.from_user.id
-
     bot.send_message(message.chat.id, f"Игра найдена")
-    bot.send_message(message.chat.id, f"Если захотите сдаться или выйти из игры напишите '/resign'")
-    bot.send_message(message.chat.id, "Вы начинаете")
-    bot.send_message(opponent[message.from_user.username]["id"], "Второй игрок присоединился. Он начинает.")
+    bot.send_message(message.chat.id, f"Если вы захотите выйти из игры до или после её начала, напишите '/leave'")
+    for i in games[game_id]["players"]:
+        bot.send_message(tele_id[i], f"К вам присоединился новый игрок: {message.from_user.username}")
+
+    games[game_id]["players"].append(message.from_user.username)
+    players[message.from_user.username] = game_id
+    tele_id[message.from_user.username] = message.from_user.id
+
+
+@bot.message_handler(commands=['play'])
+def play(message):
+    game_id = players[message.from_user.username]
+
+    if len(games[game_id]["players"]) < 2:
+        bot.send_message(message.from_user.id, "Вы не можете начать игру в одиночку. Найдите себе оппонентов.")
+        return
+
+    games[game_id]["turn"] = 0
+    beginner = games[game_id]["players"][0]
+    for i in games[game_id]["players"]:
+        bot.send_message(tele_id[i], "Игра начинается!")
+        bot.send_message(tele_id[i], f"Очерёдность: {' '.join(games[game_id]["players"])}")
+
+        if i == beginner:
+            bot.send_message(tele_id[i], "Вы начинаете")
+        else:
+            bot.send_message(tele_id[i], f"Начинает: {beginner}")
 
 
 @bot.message_handler(content_types=['text'])
@@ -138,32 +168,52 @@ def playing(message):
         game_id = players[user_name]
         turn = games[game_id]["turn"]
         city = message.text.lower()
+        group = games[game_id]["players"]
+        now = group[turn]
     except KeyError:
         return
 
-    if turn != user_name:
+    if now != user_name:
         bot.send_message(message.chat.id, "Сейчас не ваш ход!")
     else:
         if city[0] != games[game_id]["last_char"] and len(games[game_id]["cities"]) != 0:
             bot.send_message(message.chat.id,
                              f"Назвние города должно начинаться на {games[game_id]["last_char"].upper()}")
+
         elif city in games[game_id]["cities"]:
             bot.send_message(message.chat.id, f"Этот город уже был")
-            bot.send_message(opponent[message.from_user.username]["id"],
-                             f"Соперник назвал '{city}', но этот город уже был")
+
+            for i in games[game_id]["players"]:
+                if i != now:
+                    bot.send_message(tele_id[i], f"Соперник назвал '{city}', но этот город уже был")
+
         elif not (is_city_exists(city) or is_city_ai(city)):
             bot.send_message(message.chat.id, f"Этого города не существует")
-            bot.send_message(opponent[message.from_user.username]["id"],
-                             f"Соперник назвал '{city}', но этого города не существует")
+
+            for i in games[game_id]["players"]:
+                if i != now:
+                    bot.send_message(tele_id[i], f"Соперник назвал '{city}', но этого города не существует")
             bot.send_message(config.MY_CHAT_ID, f"!!!Был назван и не засчитан город '{city}'!!!")
+
         else:
             bot.send_message(message.chat.id, random.choice(answers))
             games[game_id]["cities"].append(city)
-            bot.send_message(opponent[message.from_user.username]["id"], f"Оппонент: {city.capitalize()}")
+
+            for i in games[game_id]["players"]:
+                if i != now:
+                    bot.send_message(tele_id[i], f"{now} назвал: {city.capitalize()}")
+
             games[game_id]["last_char"] = last_char(city)
-            bot.send_message(opponent[message.from_user.username]["id"],
-                             f"Вам на {games[game_id]["last_char"].upper()}")
-            games[game_id]["turn"] = opponent[message.from_user.username]["name"]
+
+            games[game_id]["turn"] += 1
+            if games[game_id]["turn"] >= len(group):
+                games[game_id]["turn"] = 0
+
+            for i in games[game_id]["players"]:
+                if i == group[games[game_id]["turn"]]:
+                    bot.send_message(tele_id[i], f"Вам на {games[game_id]["last_char"].upper()}")
+                else:
+                    bot.send_message(tele_id[i], f"Сейчас очередь {group[games[game_id]["turn"]]}")
 
 
 bot.polling(none_stop=True)
